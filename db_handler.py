72a1e33cc671c76f10f5b96ed63193caf224982b
Volodymyr Zabulskyy <v.zabulskyy@gmail.com>
2019-05-12 04:11:10 +0300

    base64_to_arr

--- db_handler.py
+++ db_handler.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import uuid
+import base64
+import numpy as np
 
 data_path = "/home/ubuntu/data/csvs/users.csv"
 df = pd.read_csv(data_path)
@@ -15,3 +17,10 @@ def get_user(user):
     else:
         uid = user.image_id.values[0]
     return uid
+
+
+def base64_to_arr(t):
+    s = base64.b64encode(t)
+    r = base64.decodebytes(s)
+    q = np.frombuffer(r, dtype=np.float64)
+    return q
