accc83e0707da4f71f6c8830e58025563eb07b3c
Volodymyr Zabulskyy <v.zabulskyy@gmail.com>
2019-05-12 03:25:08 +0300

    db handler and flask POST requests

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import jsonify
+from flask import Flask, request
+import db_handler
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/summary')
+def summary():
+    d = {'result': 'Haha'}
+    return jsonify(d)
+
+
+@app.route('/get_user', methods=['POST'])
+def login():
+    user_data = None
+    if request.method == 'POST':
+        user_data = {
+            "photo": request.form['photo'],
+            "first_name": request.form['firstname'],
+            "last_name": request.form['lastname'],
+            "email": request.form['email'],
+            "gender": request.form['gender'],
+            "pref": request.form['pref']
+        }
+    uid = db_handler.get_user(user_data)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
---
+++ db_handler.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import uuid
+
+data_path = "/home/ubuntu/data/csvs/users.csv"
+df = pd.read_csv(data_path)
+
+def add_user(user):
+    # TODO
+    return uuid.uuid4()
+
+def get_user(user):
+    user = df[df.email == user["email"]]
+    if user.empty:
+        uid = add_user(user)
+    else:
+        uid = user.image_id.values[0]
+    return uid
