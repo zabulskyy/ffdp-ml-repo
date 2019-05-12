5d67171b1a227bcf38b91e61ddf13857ba34d8af
Roman Vey <roman.vey@gmail.com>
2019-05-12 09:43:36 +0300

    aaaaaaaa

--- db_handler.py
+++ db_handler.py
@@ -4,13 +4,17 @@ import base64
 import numpy as np
 import signal
 import sys
-
-
+import scipy
+import scipy.misc
+from scipy.spatial import distance
+np.random.seed(0)
 users_csv_path = "/home/ubuntu/data/csvs/users.csv"
 celebs_csv_path = "/home/ubuntu/data/csvs/celebs.csv"
 preference_vector_path = "/home/ubuntu/data/users/preferences.npy"
 celeba_vector_path = "/home/ubuntu/data/celebs/features.npy"
 user_vector_path = "/home/ubuntu/data/users/features.npy"
+celeba_imgs_path = "/home/ubuntu/data/celebs/imgs/"
+users_imgs_path = "/home/ubuntu/data/users/imgs/"
 
 # email,first_name,gender,idx,user_id,last_name,pref_idx,pref_gender
 print("Reading database..")
@@ -21,10 +25,14 @@ celeba_vector = np.load(celeba_vector_path)
 user_vector = np.load(user_vector_path)
 print("Done!")
 
+
 def signal_handler(sig, frame):
     print("Interruption!")
     users_csv.to_csv(users_csv_path, index=False)
     celebs_csv.to_csv(celebs_csv_path, index=False)
+    np.save(preference_vector_path, preference_vector)
+    np.save(celeba_imgs_path, celeba_vector)
+    np.save(users_imgs_path, user_vector)
     print("Data saved!")
     sys.exit(0)
 
@@ -34,12 +42,18 @@ signal.signal(signal.SIGINT, signal_handler)
 
 
 def add_user(user):
-    global users_csv
+    global users_csv, preference_vector, user_vector
     uid = str(uuid.uuid4())
     idx = max(users_csv.idx) + 1
     pref_idx = -1
-    email, first_name, gender, last_name, pref_gender = user["email"], user["first_name"], user["gender"], user[
-        "last_name"], user["pref"]
+
+    email, first_name, gender, last_name, pref_gender, photo = user["email"], user["first_name"], user["gender"], user[
+        "last_name"], user["pref"], base64_to_arr(user["photo"])
+    scipy.misc.imsave(users_imgs_path + uid, photo)
+
+    preference_vector = np.stack([preference_vector, init_user_vector.reshape(1, 128)], axis=0)
+    user_vector = np.stack([user_vector, get_photo_vector(uid)], axis=0)
+
     new_data_record = pd.DataFrame([[idx,email, first_name, gender, idx, uid, last_name, pref_idx, pref_gender]],
                                    columns=users_csv.columns)
     users_csv = users_csv.append(new_data_record)
@@ -62,28 +76,56 @@ def base64_to_arr(t):
     return q
 
 
+def cos_cdist(vector, matrix):
+    """
+    Compute the cosine distances between each row of matrix and vector.
+    """
+    vector = vector.reshape(1, -1)
+    return scipy.spatial.distance.cdist(matrix, vector, 'cosine').reshape(-1)
+
+
 def get_img(user_id):
-    """
-
-    :param user_id: id of user for which we found 6 matches (from top 100) celebs
-    :param all_data: csv user data (email,first_name,gender,idx,image_id,last_name,pref_idx,pref_gender)
-    :param vector: users preference vector
-    :param celebs: csv celebs (celeb_id, gender ...)
-    :param celeba_vector: celebs preference vector
-
-    :return: list, 6 celebs - [base64, celeb_id]
-    """
     # take vector of preferences and gender preference
     found = users_csv[users_csv.user_id == user_id]
 
     p_gender = found['pref_gender']
-    p_idx = found['pref_idx']
 
+    p_idx = found['pref_idx']
     p_vector = preference_vector[p_idx]
 
-    if(p_gender != 2):
-        interested = celebs_csv[celebs_csv.gender == p_gender]
+    if p_gender != 2:
+        interested_celeb_idxs = celebs_csv[celebs_csv.gender == p_gender].idx.reset_index(drop=True)
     else:
-        interested = celebs_csv
+        interested_celeb_idxs = celebs_csv.idx.reset_index(drop=True)
+    interested_celeb_vectors = celeba_vector[interested_celeb_idxs]
+
+    cosine_distance = cos_cdist(p_vector, interested_celeb_vectors)
+    idxs = np.argsort(cosine_distance)
+
+    selected = np.random.choice(idxs[-100:], 6, replace=False)
+    interested_celeb_idxs = list(interested_celeb_idxs[selected])
+    celeba_data = celebs_csv.iloc[interested_celeb_idxs, :]
+    output = []
+    for i, row in celeba_data.iterrows():
+        with open('{}{}.jpg'.format(celeba_imgs_path, row['face_id']), 'rb') as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        output.append({
+            'photo': encoded_string,
+            'id': row['face_id']
+        })
+    return {
+        'images': output
+    }
+
+
+def init_user_vector(count=10):
+    idx = np.arange(0, celeba_vector.shape[0])
+    choiced = np.random.choice(idx, count, replace=False)
+    return celeba_vector[choiced].mean()
+
+
+def get_photo_vector(uid):
+    pass
+
 
 print("Import finished!")
